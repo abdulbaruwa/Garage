6f680a72516a8a592564b1e9797130df7c220b7f
abdul baruwa <abdulbaruwa@hotmail.com>
2013-05-08 05:48:56 +0100

    Max and Min flow graph implementation

--- AlgorithmsJournal/AlgorithmsJournal/Graphs/Edge.cs
+++ AlgorithmsJournal/AlgorithmsJournal/Graphs/Edge.cs
@@ -37,4 +37,62 @@ namespace AlgorithmsJournal.Graphs
 
 
     }
+
+
+
+    public class FlowEdge : IComparable
+    {
+        private readonly double _capacity;
+
+        public FlowEdge(int fromVertex, int toVertex, double capacity)
+        {
+            From = fromVertex;
+            To = toVertex;
+            Capacity = capacity;
+        }
+
+        public int CompareTo(object item)
+        {
+            var obj = (FlowEdge)item;
+            if (_capacity < obj._capacity)
+                return -1;
+            if (_capacity > obj._capacity)
+                return +1;
+            return 0;
+        }
+
+        public int From { get; private set; }
+        public int To { get; private set; }
+        public double Capacity { get; private set; }
+        public int Other(int vertex)
+        {
+            throw new NotImplementedException();
+        }
+        public double Flow()
+        {
+            throw new NotImplementedException();
+        }
+
+        public double ResidualCapacityTo(int vertex)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddResidualFlowTo(int vertex, double delta)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        //v--------7/9-------->w
+        //7 = flow fe
+        //9 = capacitye ce
+
+        //v--------2 -------->w
+        // <--------7 -------
+        //residual capacity = 2, on forward edge
+        //residual capacity = 7, on backward edge
+        
+
+    }
 }
